82bcee2a49050f5d8d9e18de3ab9af690fbb7783
Safa Mohammed <safa00almahrouqi@gmail.com>
2025-09-14 01:54:10 +0400

    update codes css auth paages

--- src/modules/Authontication/Components/Login/Login.tsx
+++ src/modules/Authontication/Components/Login/Login.tsx
@@ -1,9 +1,141 @@
-import React from 'react'
+// src/pages/Login.tsx
+import React from "react";
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Link,
+  InputAdornment,
+  IconButton,
+  
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
-export default function Login() {
-  return (
-    <div>
-      login
-    </div>
-  )
+interface LoginFormInputs {
+  email: string;
+  password: string;
 }
+
+const Login: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>();
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
+    try {
+      const response = await axios.post(
+        "https://upskilling-egypt.com:3000/api/v0/admin/users/login",
+        data
+      );
+      toast.success("✅ Login successful!");
+      console.log("Response:", response.data);
+      // You can store token in localStorage if needed:
+      // localStorage.setItem("token", response.data.token);
+    } catch (error: any) {
+      if (error.response?.data?.message) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("❌ Something went wrong. Try again!");
+      }
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        // minHeight: "100vh",
+         display: "flex",
+         flexDirection:"column",
+         height:'30px',
+        // alignItems: "center",
+        // justifyContent: "center",
+        // bgcolor: "#f5f5f5",
+         p: 4,
+      }}
+    >
+     
+        <Typography variant="h5" fontWeight="bold" gutterBottom>
+          Sign in
+        </Typography>
+
+        <Typography variant="body2" sx={{ }}>
+          If you don’t have an account register <br />
+          <Link href="#" underline="hover" fontWeight="bold" color="primary">
+            Register here !
+          </Link>
+        </Typography>
+        <br />
+
+        {/* Form */}
+            <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate>
+      {/* Email */}
+     <TextField
+  fullWidth={false}        // disable fullWidth to use custom width
+  label="Email Address"
+  placeholder="Enter your email..."
+  margin="normal"
+  sx={{
+    width: '100%',            // fixed width in pixels
+    height: "60px",
+  }}
+  {...register("email", { required: "Email is required" })}
+  error={!!errors.email}
+  helperText={errors.email?.message}
+  InputLabelProps={{ shrink: true }}
+/>
+
+
+      {/* Password */}
+      <TextField
+        fullWidth
+        label="Password"
+        placeholder="Enter your password..."
+        type={showPassword ? "text" : "password"}
+        margin="normal"
+         sx={{          
+    height: "60px",
+  }}
+        {...register("password", { required: "Password is required" })}
+        error={!!errors.password}
+        helperText={errors.password?.message}
+        InputLabelProps={{ shrink: true }} 
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPassword((prev) => !prev)}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      {/* Submit Button */}
+      <Button
+        type="submit"
+        variant="contained"
+        fullWidth
+        sx={{ mt: 2, borderRadius: 2, py: 3 }}
+      >
+        Login
+      </Button>
+    </Box>
+
+        
+       
+    </Box>
+  );
+};
+
+export default Login;
--- src/modules/Shared/Components/AuthLayout/AuthLayout.tsx
+++ src/modules/Shared/Components/AuthLayout/AuthLayout.tsx
@@ -1,9 +1,160 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import { Outlet, useLocation } from "react-router-dom";
+import myImage from "../../../../assets/images/Rectangle 7.png";
+import logo from "../../../../assets/images/Staycation..svg";
+import Login from "../../../Authontication/Components/Login/Login";
 
 export default function AuthLayout() {
+  const location = useLocation();
+  
+  // Text in img depend in route of pages
+  const getHeaderText = () => {
+    if ( location.pathname.includes("login")) {
+      return "Sign up to Roamhome";
+    } else if (location.pathname.includes("forgot-password")) {
+      return "Forgot password";
+    }else if (location.pathname.includes("changePassword")) {
+      return "Change password";
+    } else if (location.pathname.includes("resetPassword")) {
+      return "Reset password";
+    }else {
+      return "Sign in to Roamhome";
+    }
+  };
+
+  const getSubText = () => {
+    if (location.pathname.includes("signup") || location.pathname.includes("register")) {
+      return "Join our community today.";
+    } else if (location.pathname.includes("forgot-password")) {
+      return "We'll help you get back in.";
+    } else {
+      return "Homes as unique as you.";
+    }
+  };
+
   return (
-    <div>
-      AuthLayout
-    </div>
-  )
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column-reverse", md: "row" },
+        height: "100vh",
+        margin: 0,
+        overflow: "hidden", 
+      }}
+    >
+      {/* Form Section */}
+    <Box
+  sx={{
+    flex: 1,
+    backgroundColor: "#fff",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    width: { xs: "100%", md: "50%" },
+    // height: "70%",  
+    padding: { xs: 2, md: 4 },
+    overflowY: "auto", // allow scroll if content is taller
+  }}
+>
+
+        {/* Logo */}
+        <Box
+          sx={{
+            width: "100%",
+            height: '15%',
+            paddingTop: { xs: 1, md: 1 },
+            paddingBottom: 3,
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          <img
+            src={logo}
+            alt="Staycation Logo"
+            style={{
+              maxWidth: "148px",
+              maxHeight: "39px",
+              height: "auto",
+            }}
+          />
+        </Box>
+
+        {/* Form content */}
+        <Box sx={{ width: "100%"  }}>
+          
+          <Login />
+        </Box>
+      </Box>
+
+      {/* Image Section */}
+      <Box
+        sx={{
+          flex: 1,
+          width: { xs: "100%", md: "50%" },
+          height: { xs: "40vh", md: "100%" },
+          order: { xs: -1, md: 1 },
+          position: "relative",
+        }}
+      >
+        <img
+          src={myImage}
+          alt="Auth Illustration"
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            display: "block", 
+            paddingTop: '49px',
+          }}
+        />
+
+        {/* Overlay Text */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: "80%",
+            left: "50%",
+            height: '80px',
+            width: '100%',
+            transform: "translate(-50%, -50%)",
+            color: "white",
+            textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            textAlign: "left",
+            maxWidth: { xs: "80%", sm: "70%", md: "70%" },
+          }}
+        >
+          <Typography
+            component="h3"
+            sx={{
+              margin: 0,
+              fontFamily: "Poppins, sans-serif",
+              fontWeight: 600,
+              fontSize: { xs: "24px", sm: "32px", md: "40px" },
+              lineHeight: 1.2,
+              letterSpacing: "1px",
+            }}
+          >
+            {getHeaderText()}
+          </Typography>
+
+          <Typography
+            component="p"
+            sx={{
+              margin: 0,
+              fontFamily: "Poppins, sans-serif",
+              fontWeight: 500,
+              fontSize: { xs: "14px", sm: "18px", md: "20px" },
+              letterSpacing: "1px",
+            }}
+          >
+            {getSubText()}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
