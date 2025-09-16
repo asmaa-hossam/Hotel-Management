38da5e9024eaf212da014c260bde950dd299efc2
Safa Mohammed <safa00almahrouqi@gmail.com>
2025-09-16 21:20:22 +0400

    master layout

--- src/modules/Shared/Components/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/Components/MasterLayout/MasterLayout.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "../Navbar/Navbar";
+import SideBar from "../SideBar/SideBar";
 
 export default function MasterLayout() {
   return (
-    <div>
-      MasterLayout
-    </div>
-  )
+    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+      {/* Sidebar */}
+      <Box
+        sx={{
+          width: 240,
+          bgcolor: "primary.main",
+          color: "white",
+          p: 2,
+        }}
+      >
+        <SideBar />
+      </Box>
+
+      {/* Main Content */}
+      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+        {/* Top Navbar */}
+        <Box
+          sx={{
+            height: 64,
+            bgcolor: "secondary.main",
+            color: "white",
+            display: "flex",
+            alignItems: "center",
+            px: 2,
+          }}
+        >
+          <Navbar />
+        </Box>
+
+        {/* Page Content */}
+        <Box sx={{ flexGrow: 1, p: 2 }}>
+          <Outlet />
+        </Box>
+      </Box>
+    </Box>
+  );
 }
