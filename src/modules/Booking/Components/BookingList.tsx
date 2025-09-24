5e03ab301acbfeb61fd38a19353d9fd5cafafcca
Asmaa Ahmed <asmaahmed2023@gmail.com>
2025-09-25 02:01:36 +0300

    Revert "Merge branch 'development' into asmanewbranch"
    This reverts commit 25916fb4aa416ab445d9043a15bfb2604d638095, reversing
    changes made to 7e948ddd08ce9ed965f52f0f64448f1ae15a3545.

--- src/modules/Booking/Components/BookingList.tsx
+++ src/modules/Booking/Components/BookingList.tsx
@@ -1,18 +1,11 @@
-
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import axios from "axios";
 import type { AxiosResponse } from "axios";
-import NoData from "../../Shared/Components/NoData/NoData";
-import { Facilities_URL } from "../../../services/urls";
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import CloseIcon from '@mui/icons-material/Close';
-import Loader from "../../Shared/Components/Loader/Loader";
+import {BOOKING_URL} from "../../../services/urls";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { toast } from "react-toastify";
 import {
   Box,
   Button,
@@ -29,17 +22,14 @@ import {
   Container,
   TextField,
   CircularProgress,
-  Modal
+  Modal,
+  TablePagination,
 } from "@mui/material";
 
 
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
-import Tooltip from "@mui/material/Tooltip";
-import { toast } from "react-toastify";
-// ---- Table Styles ----
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
+
+//Table Styles 
+const StyledTableCell = styled(TableCell)(({}) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: "#E2E5EB",
     color: " #1F263E",
@@ -51,9 +41,12 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
+const StyledTableRow = styled(TableRow)(() => ({
   "&:nth-of-type(odd)": {
     backgroundColor: "#ffff",
+  },
+    "&:nth-of-type(even)": {
+    backgroundColor: "#F8F9FB",
   },
   // hide last border
   "&:last-child td, &:last-child th": {
@@ -61,174 +54,59 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     // background: "#F8F9FB",
   },
 }));
+
+
+
+
 export default function BookingList() {
-    type Facility = {
+
+    type BookingList = {
     _id: string,
-    name: string,
-    createdAt: string,
-    updatedAt: string,
+    totalPrice:string,
+    Active:string
+    endDate:string,
+    startDate:string,
+    User:string,
+    room:{
+      roomNumber:string},
+    user :{
+     userName:string,} 
   };
 
-  type CustomizedDialogsProps = {
-    open: boolean;
-    handleClose: () => void;
-  };
+ 
 
-  const [FacilitiesList, setFacilities] = useState<Facility[]>([]);
-  const [openDialog, setOpenDialog] = React.useState(false);
+  const [BookingList, setBookingList] = useState<BookingList[]>([]);
   const [loading, setLoading] = React.useState(true);
-  const handleOpenDialog = () => setOpenDialog(true);
-  const handleCloseDialog = () => setOpenDialog(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const[total,setTotal]  =useState(0)
+  const [open, setOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState<any>(null);
+  const handleClose = () => setOpen(false);
   const token = localStorage.getItem("token");
 
 
-
-
-  
-
-  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-      padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-      padding: theme.spacing(1),
-    },
-  }));
-
-  function CustomizedDialogs({ open, handleClose }: CustomizedDialogsProps) {
-
-    const [facilityName, setFacilityName] = useState<string>("");
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-      try {
-
-        const response: AxiosResponse = await axios.post(
-          Facilities_URL.CREATE,
-          { name: facilityName }, // request body
-          {
-            headers: {
-              Authorization: `${token}`, // if your API needs auth
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        console.log("✅ Facility added:", response.data);
-         toast.success(response?.data?.message)
-
-        await getFacilities(token);
-        handleClose(); // close modal after success
-        } 
-       catch (error:any) {
-        console.error("Error adding facility:", error);
-         toast.error(error.response?.data?.message)
-      }
-
-    };
-
-    return (
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-
-        <Box sx={{ m: 0, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-          <DialogTitle id="customized-dialog-title">Add Facility</DialogTitle>
-
-          <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={{ color: "#151414ff" }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Box>
-
-
-
-
-        <form onSubmit={handleSubmit}>
-          <DialogContent >
-            <TextField
-              fullWidth
-              label="Name"
-              variant="outlined"
-              margin="normal"
-              value={facilityName}
-              required
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setFacilityName(e.target.value)
-              }
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "12px", // rounded corners
-                  backgroundColor: "#f9f9f9", // light background
-                  "& fieldset": {
-                    borderColor: "#ccc", // default border
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#203FC7", // hover border color
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#203FC7", // focus border color
-                    borderWidth: "2px",
-                  },
-                },
-                "& .MuiInputLabel-root": {
-                  fontFamily: "Poppins, sans-serif",
-                  fontWeight: 500,
-                  fontSize: "14px",
-                  color: "#323C47",
-                },
-                "& .MuiInputBase-input": {
-                  fontFamily: "Poppins, sans-serif",
-                  fontSize: "14px",
-                  padding: "12px 14px",
-                },
-              }}
-            />
-
-
-
-          </DialogContent>
-          <DialogActions>
-            <Button type="submit" variant="contained"
-              sx={{
-                textTransform: "none",
-                borderRadius: "8px",
-                backgroundColor: "#203FC7",
-                fontWeight: "bold",
-                width: "20px",
-              }} autoFocus >
-              Save
-            </Button>
-          </DialogActions>
-
-        </form>
-      </BootstrapDialog>
-    );
-  }
-
   // GET ALL FACILITIES 
-  async function getFacilities(token: string | null): Promise<void> {
+  async function getBookingList(token: string  | null, page = 1, size = 10): Promise<void> {
     try {
 
       setLoading(true)
       if (!token) return console.warn("No token provided");
+      console.log("Fetching from:", BOOKING_URL.GETALL);
 
-      const response: AxiosResponse = await axios.get(Facilities_URL.GETALL, {
+      const response: AxiosResponse = await axios.get(`${BOOKING_URL.GETALL}?page=${page}&size=${size}` ,
+        {
         headers: { Authorization: `${token}` },
-      });
+        });
 
-      console.log("Full response:", response.data.data.facilities);
+      console.log("Full response:", response);
 
-      setFacilities(response.data.data.facilities)
-
+      setBookingList(response.data.data.booking)
+      setTotal(response.data.data.totalCount);
+     
     } catch (error) {
-      console.error("Error fetching facilities:", error);
-      toast.error("Error fetching facilities:")
+      console.error("Error fetching BookingList", error);
+      toast.error("Error fetching BookingList")
     }
     finally {
       setLoading(false)
@@ -236,29 +114,12 @@ export default function BookingList() {
   }
 
   useEffect(() => {
-    getFacilities(token);
+    getBookingList(token);
   }, [token]);
    
-
-
-const modalStyle = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  borderRadius: "10px",
-  boxShadow: 24,
-  p: 4,
-};
-
-function FacilityActions() {
-  const [open, setOpen] = useState(false);
-  const [actionType, setActionType] = useState<"view" | "edit" | "delete" | null>(null);
-
-  const handleOpen = (type: "view" | "edit" | "delete") => {
-    setActionType(type);
+  const handleOpen = (row: any) =>
+  {
+     
     setOpen(true);
     setSelectedRow(row)
     console.log("clicked row:", row); 
@@ -267,43 +128,29 @@ function FacilityActions() {
 
 
   return (
-    <Container maxWidth="xl"
-      sx={{
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        overflow: "hidden", // prevents scroll
-      }}>
 
-      {/* <BookingListHeader /> */}
-      {!loading && (
-        <Container
-          maxWidth={false}   // disables max-width restriction
-          sx={{
-            width: "100%",
-            px: 2,           // optional horizontal padding
-          }}
-        >
+    <Container
+  maxWidth="xl"
+>
+  {/* Header Section */}
+  <Container
+    maxWidth={false}
+    sx={{
+      width: "100%",
+      px: 2,
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      mb:2
+    }}
+  >
+    <Box >
+      <Typography variant="h5">Booking Table Details</Typography>
+      <Typography variant="body1">You can check all details</Typography>
+    </Box>
+  </Container>
 
-          <Box sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            mb: 3,
-          }}>
-            <Box>
-              <Typography variant="h5">Booking Table Details</Typography>
-              <Typography variant="body1">You can check all details</Typography>
-            </Box>
-          
-
-          </Box>
-
-        </Container>)}
-      {/* Table Section */}
+  {/* Table Section */}
 
 <TableContainer
   component={Paper}
