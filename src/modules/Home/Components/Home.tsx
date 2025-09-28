d00ed66359021d9a7c9fd49b8fd9322b93927f06
asmaa <hossamasmaa07@gmail.com>
2025-09-28 17:51:07 +0300

    review

--- src/modules/Home/Components/Home.tsx
+++ src/modules/Home/Components/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import CalenderBooking from '../../UsersPortal/Components/UsersShared/CalenderBooking/CalenderBooking'
 import Houses from '../../UsersPortal/Components/UsersShared/Houses/Houses'
 import Hotels from '../../UsersPortal/Components/UsersShared/Hotels/Hotels'
@@ -9,7 +8,6 @@ import Ads from '../../UsersPortal/Components/UsersShared/Ads/Ads'
 export default function Home() {
   return (
     <div>
-home
 <CalenderBooking/>
 <MostPopularAds/>
 <Houses/>
